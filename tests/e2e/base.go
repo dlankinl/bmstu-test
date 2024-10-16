c431ed7d551c07c8ec235ceade27768e2bb1b401
dmitry <dmit.lankin@mail.ru>
2024-10-16 12:36:01 +0300

    remake of e2e

--- tests/e2e/base.go
+++ tests/e2e/base.go
@@ -2,6 +2,171 @@
 
 package e2e
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/go-chi/jwtauth/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"ppo/internal/app"
+	"ppo/internal/config"
+	"ppo/internal/storage"
+	loggerPackage "ppo/pkg/logger"
+	"ppo/web"
+)
 
+var tokenAuth *jwtauth.JWTAuth
 var TestDbInstance *pgxpool.Pool
+
+func RunTheApp(db *pgxpool.Pool, done chan os.Signal) {
+	cfg, err := config.ReadConfig()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	logDir := "logs"
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		log.Println("создание директории для логов:", err)
+	}
+
+	logPath := filepath.Join(logDir, "test_logs.log")
+	logFileW, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println("Failed to open log file:", err)
+		return
+	}
+	defer logFileW.Close()
+
+	logger := loggerPackage.NewLogger(cfg.Logger.Level, logFileW)
+	if err != nil {
+		log.Fatalln("cоздание логгера:", err)
+	}
+
+	tokenAuth = jwtauth.New("HS256", []byte(cfg.Server.JwtKey), nil)
+
+	a := app.NewApp(db, cfg, logger)
+
+	mux := chi.NewMux()
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	mux.Use(middleware.Logger)
+
+	mux.Route("/entrepreneurs", func(r chi.Router) {
+		r.Get("/{id}", web.GetEntrepreneur(a))
+		r.Get("/", web.ListEntrepreneurs(a))
+
+		r.Group(func(r chi.Router) {
+			r.Use(jwtauth.Verifier(tokenAuth))
+			r.Use(jwtauth.Authenticator(tokenAuth))
+			r.Use(web.ValidateAdminRoleJWT)
+
+			r.Patch("/{id}/update", web.UpdateEntrepreneur(a))
+			r.Delete("/{id}/delete", web.DeleteEntrepreneur(a))
+		})
+	})
+
+	mux.Route("/activity_fields", func(r chi.Router) {
+		r.Get("/{id}", web.GetActivityField(a))
+		r.Get("/", web.ListActivityFields(a))
+
+		r.Group(func(r chi.Router) {
+			r.Use(jwtauth.Verifier(tokenAuth))
+			r.Use(jwtauth.Authenticator(tokenAuth))
+			r.Use(web.ValidateAdminRoleJWT)
+
+			r.Post("/create", web.CreateActivityField(a))
+			r.Patch("/{id}/update", web.UpdateActivityField(a))
+			r.Delete("/{id}/delete", web.DeleteActivityField(a))
+		})
+	})
+
+	mux.Route("/companies", func(r chi.Router) {
+		r.Get("/{id}", web.GetCompany(a))
+		r.Get("/", web.ListEntrepreneurCompanies(a))
+
+		r.Group(func(r chi.Router) {
+			r.Use(jwtauth.Verifier(tokenAuth))
+			r.Use(jwtauth.Authenticator(tokenAuth))
+			r.Use(web.ValidateUserRoleJWT)
+
+			r.Post("/create", web.CreateCompany(a))
+			r.Patch("/{id}/update", web.UpdateCompany(a))
+			r.Delete("/{id}/delete", web.DeleteCompany(a))
+		})
+
+		r.Route("/{id}/financials", func(r chi.Router) {
+			r.Use(jwtauth.Verifier(tokenAuth))
+			r.Use(jwtauth.Authenticator(tokenAuth))
+			r.Use(web.ValidateUserRoleJWT)
+
+			r.Post("/create", web.CreateReport(a))
+			r.Get("/{year-start}_{quarter-start}-{year-end}_{quarter-end}", web.ListCompanyReports(a))
+		})
+	})
+
+	mux.Route("/financials", func(r chi.Router) {
+		r.Group(func(r chi.Router) {
+			r.Use(jwtauth.Verifier(tokenAuth))
+			r.Use(jwtauth.Authenticator(tokenAuth))
+			r.Use(web.ValidateUserRoleJWT)
+
+			r.Delete("/{id}/delete", web.DeleteFinReport(a))
+			r.Patch("/{id}/update", web.UpdateFinReport(a))
+		})
+	})
+
+	mux.Post("/login", web.LoginHandler(a))
+	mux.Post("/signup", web.RegisterHandler(a))
+
+	go func() {
+		metricsAddress := fmt.Sprintf("%s:%s", cfg.Server.MetricsHost, cfg.Server.MetricsPort)
+
+		metricsMux := http.NewServeMux()
+		metricsMux.Handle("/metrics", promhttp.Handler())
+
+		fmt.Printf("сервер метрик прослушивает адрес: %s\n", metricsAddress)
+		http.ListenAndServe(metricsAddress, metricsMux)
+	}()
+
+	go func() {
+		serverAddress := fmt.Sprintf("%s:8083", cfg.Server.ServerHost)
+		fmt.Printf("сервер прослушивает адрес: %s\n", serverAddress)
+		logger.Infof("сервер прослушивает адрес: %s\n", serverAddress)
+		http.ListenAndServe(serverAddress, mux)
+	}()
+
+	<-done
+
+}
+
+func newConn(ctx context.Context, cfg *config.Database) (pool storage.DBConn, err error) {
+	connStr := fmt.Sprintf("%s://%s:%s@%s:%s/%s", cfg.Driver, cfg.User, cfg.Password,
+		cfg.Host, cfg.Port, cfg.Name)
+
+	pool, err = pgxpool.New(ctx, connStr)
+	if err != nil {
+		return nil, fmt.Errorf("подключение к БД: %w", err)
+	}
+
+	err = pool.Ping(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("пинг БД: %w", err)
+	}
+
+	return pool, nil
+}
--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -1,22 +1,17 @@
-//go:build e2e
+///go:build e2e
 
 package e2e
 
 import (
-	"context"
-	"github.com/google/uuid"
+	"github.com/gavv/httpexpect/v2"
 	"github.com/ozontech/allure-go/pkg/framework/provider"
 	"github.com/ozontech/allure-go/pkg/framework/suite"
 	"go.uber.org/mock/gomock"
+	"net/http"
+	"os"
 	"ppo/domain"
-	"ppo/internal/services/activity_field"
-	"ppo/internal/services/auth"
-	"ppo/internal/services/company"
-	"ppo/internal/services/fin_report"
-	"ppo/internal/services/user"
-	"ppo/internal/storage/postgres"
-	"ppo/internal/utils"
 	"ppo/mocks"
+	"time"
 )
 
 type E2ESuite struct {
@@ -30,36 +25,134 @@ type E2ESuite struct {
 	auSvc  domain.IAuthService
 	cSvc   domain.ICompanyService
 	fSvc   domain.IFinancialReportService
+
+	e httpexpect.Expect
 }
 
 func (s *E2ESuite) BeforeAll(t provider.T) {
 	s.ctrl = gomock.NewController(t)
 
 	t.Title("[e2e] init test repository")
-	aRepo := postgres.NewActivityFieldRepository(TestDbInstance)
-	uRepo := postgres.NewUserRepository(TestDbInstance)
-	auRepo := postgres.NewAuthRepository(TestDbInstance)
-	cRepo := postgres.NewCompanyRepository(TestDbInstance)
-	fRepo := postgres.NewFinReportRepository(TestDbInstance)
-
+	//aRepo := postgres.NewActivityFieldRepository(TestDbInstance)
+	//uRepo := postgres.NewUserRepository(TestDbInstance)
+	//auRepo := postgres.NewAuthRepository(TestDbInstance)
+	//cRepo := postgres.NewCompanyRepository(TestDbInstance)
+	//fRepo := postgres.NewFinReportRepository(TestDbInstance)
+	//
 	s.logger = mocks.NewMockILogger(s.ctrl)
 	s.crypto = mocks.NewMockIHashCrypto(s.ctrl)
-	s.aSvc = activity_field.NewService(aRepo, cRepo, s.logger)
-	s.uSvc = user.NewService(uRepo, cRepo, aRepo, s.logger)
-	s.auSvc = auth.NewService(auRepo, s.crypto, "jwt123", s.logger)
-	s.cSvc = company.NewService(cRepo, aRepo, s.logger)
-	s.fSvc = fin_report.NewService(fRepo, s.logger)
+	//s.aSvc = activity_field.NewService(aRepo, cRepo, s.logger)
+	//s.uSvc = user.NewService(uRepo, cRepo, aRepo, s.logger)
+	//s.auSvc = auth.NewService(auRepo, s.crypto, "jwt123", s.logger)
+	//s.cSvc = company.NewService(cRepo, aRepo, s.logger)
+	//s.fSvc = fin_report.NewService(fRepo, s.logger)
+
+	s.e = *httpexpect.WithConfig(httpexpect.Config{
+		Client:   &http.Client{},
+		BaseURL:  "http://localhost:8083",
+		Reporter: httpexpect.NewAssertReporter(t),
+		Printers: []httpexpect.Printer{
+			httpexpect.NewDebugPrinter(t, true),
+		},
+	})
 
 	t.Tags("fixture", "e2e")
+	done := make(chan os.Signal, 1)
+	go RunTheApp(TestDbInstance, done)
 }
 
-func (s *E2ESuite) Test(t provider.T) {
-	t.Title("[e2e] Test")
+//func (s *E2ESuite) Test(t provider.T) {
+//	t.Title("[e2e] Test")
+//	t.Tags("e2e", "postgres")
+//	t.Parallel()
+//	t.WithNewStep("Success", func(sCtx provider.StepCtx) {
+//		ctx := context.TODO()
+//
+//		s.logger.EXPECT().
+//			Infof(gomock.Any()).
+//			AnyTimes()
+//		s.logger.EXPECT().
+//			Infof(gomock.Any(), gomock.Any()).
+//			AnyTimes()
+//		s.logger.EXPECT().
+//			Warnf(gomock.Any(), gomock.Any()).
+//			AnyTimes()
+//		s.logger.EXPECT().
+//			Errorf(gomock.Any(), gomock.Any()).
+//			AnyTimes()
+//
+//		s.crypto.EXPECT().
+//			CheckPasswordHash("newUserPass", "pass123").
+//			Return(true)
+//
+//		s.crypto.EXPECT().
+//			GenerateHashPass("newUserPass").
+//			Return("pass123", nil)
+//
+//		userAuth := utils.NewUserAuthBuilder().
+//			WithUsername("newUser").
+//			WithPassword("newUserPass").
+//			Build()
+//
+//		loginUserAuth := utils.NewUserAuthBuilder().
+//			WithUsername("newUser").
+//			WithPassword("newUserPass").
+//			WithHashedPass("pass123").
+//			Build()
+//
+//		id, regErr := s.auSvc.Register(ctx, &userAuth)
+//
+//		_, logErr := s.auSvc.Login(ctx, &loginUserAuth)
+//
+//		actFieldId, _ := uuid.Parse("fa406cca-27d6-446e-8cfd-b1a71ed680a0")
+//
+//		company := utils.NewCompanyBuilder().
+//			WithName("newCompany").
+//			WithCity("newCity").
+//			WithOwner(id).
+//			WithActivityField(actFieldId).
+//			Build()
+//
+//		comp, compCreateErr := s.cSvc.Create(ctx, &company)
+//
+//		report := utils.NewFinReportBuilder().
+//			WithQuarter(1).
+//			WithYear(1).
+//			WithCompanyID(comp.ID).
+//			WithCosts(100).
+//			WithRevenue(200).
+//			Build()
+//
+//		reportCreateErr := s.fSvc.Create(ctx, &report)
+//
+//		period := utils.NewPeriodBuilder().
+//			WithStartYear(1).
+//			WithEndYear(1).
+//			WithStartQuarter(1).
+//			WithEndQuarter(1).
+//			Build()
+//
+//		reports, getByCompanyErr := s.fSvc.GetByCompany(ctx, comp.ID, &period)
+//
+//		expectedReports := utils.NewFinReportByPeriodBuilder().
+//			WithReports([]domain.FinancialReport{report}).
+//			WithPeriod(period).
+//			Build()
+//
+//		sCtx.Assert().NoError(regErr)
+//		sCtx.Assert().NoError(logErr)
+//		sCtx.Assert().NoError(compCreateErr)
+//		sCtx.Assert().NoError(reportCreateErr)
+//		sCtx.Assert().NoError(getByCompanyErr)
+//		sCtx.Assert().Equal(&expectedReports, reports)
+//	})
+//}
+
+func (s *E2ESuite) Test2(t provider.T) {
+	t.Title("[e2e] Test2")
 	t.Tags("e2e", "postgres")
 	t.Parallel()
 	t.WithNewStep("Success", func(sCtx provider.StepCtx) {
-		ctx := context.TODO()
-
 		s.logger.EXPECT().
 			Infof(gomock.Any()).
 			AnyTimes()
@@ -81,61 +174,30 @@ func (s *E2ESuite) Test(t provider.T) {
 			GenerateHashPass("newUserPass").
 			Return("pass123", nil)
 
-		userAuth := utils.NewUserAuthBuilder().
-			WithUsername("newUser").
-			WithPassword("newUserPass").
-			Build()
+		type Req struct {
+			Login    string `json:"login"`
+			Password string `json:"password"`
+		}
+		req := Req{"newUser2", "newUserPass"}
 
-		loginUserAuth := utils.NewUserAuthBuilder().
-			WithUsername("newUser").
-			WithPassword("newUserPass").
-			WithHashedPass("pass123").
-			Build()
+		s.e.POST("/signup").
+			WithJSON(req).
+			Expect().
+			Status(http.StatusOK).
+			JSON().
+			Object().
+			NotEmpty().
+			HasValue("status", "success")
 
-		id, regErr := s.auSvc.Register(ctx, &userAuth)
+		time.Sleep(time.Second)
 
-		_, logErr := s.auSvc.Login(ctx, &loginUserAuth)
-
-		actFieldId, _ := uuid.Parse("fa406cca-27d6-446e-8cfd-b1a71ed680a0")
-
-		company := utils.NewCompanyBuilder().
-			WithName("newCompany").
-			WithCity("newCity").
-			WithOwner(id).
-			WithActivityField(actFieldId).
-			Build()
-
-		comp, compCreateErr := s.cSvc.Create(ctx, &company)
-
-		report := utils.NewFinReportBuilder().
-			WithQuarter(1).
-			WithYear(1).
-			WithCompanyID(comp.ID).
-			WithCosts(100).
-			WithRevenue(200).
-			Build()
-
-		reportCreateErr := s.fSvc.Create(ctx, &report)
-
-		period := utils.NewPeriodBuilder().
-			WithStartYear(1).
-			WithEndYear(1).
-			WithStartQuarter(1).
-			WithEndQuarter(1).
-			Build()
-
-		reports, getByCompanyErr := s.fSvc.GetByCompany(ctx, comp.ID, &period)
-
-		expectedReports := utils.NewFinReportByPeriodBuilder().
-			WithReports([]domain.FinancialReport{report}).
-			WithPeriod(period).
-			Build()
-
-		sCtx.Assert().NoError(regErr)
-		sCtx.Assert().NoError(logErr)
-		sCtx.Assert().NoError(compCreateErr)
-		sCtx.Assert().NoError(reportCreateErr)
-		sCtx.Assert().NoError(getByCompanyErr)
-		sCtx.Assert().Equal(&expectedReports, reports)
+		s.e.POST("/login").
+			WithJSON(req).
+			Expect().
+			Status(http.StatusOK).
+			JSON().
+			Object().
+			NotEmpty().
+			HasValue("status", "success")
 	})
 }
